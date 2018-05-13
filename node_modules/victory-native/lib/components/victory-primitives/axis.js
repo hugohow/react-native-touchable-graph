2a4b62559422328eede35ed98ccf26ba8cab1967
hugohow <hugo@lajavaness.com>
2018-05-13 18:32:04 +0200

    Delete node_modules

--- node_modules/victory-native/lib/components/victory-primitives/axis.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Line } from "../../index";
-import { Axis } from "victory-core/es";
-
-export default class extends Axis {
-  static defaultProps = Object.assign({}, Axis.defaultProps, {
-    lineComponent: <Line/>
-  });
-}
